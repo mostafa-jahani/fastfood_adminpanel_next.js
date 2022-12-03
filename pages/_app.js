684bcb84f0206704475d18a93059ceff28c11e6c
Mostafa Jahani <mm.mostafa.mm@gmail.com>
2022-12-03 15:35:06 +0330

    start project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.rtl.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/globals.css'
+import {useEffect} from "react";
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.js')
+  }, []);
   return <Component {...pageProps} />
 }
 
