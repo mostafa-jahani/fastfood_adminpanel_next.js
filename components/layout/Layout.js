6e7f4fa28d68df9ddd49e04eba167b6f25fb4ce0
Mostafa Jahani <mm.mostafa.mm@gmail.com>
2022-12-03 23:25:06 +0330

    create login page template

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,17 +1,25 @@
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
+import {ToastContainer} from "react-toastify";
+import {useRouter} from "next/router";
 
 
 const Layout = ({children}) => {
+
+    const router = useRouter()
+
+    if (router.pathname === "/auth/login") {
+        return (
+            <div>{children}</div>
+        )
+    }
+
     return (
         <>
-            <Header />
-
+            <Header/>
             <div className="container-fluid">
                 <div className="row">
-
-                    <Sidebar />
-
+                    <Sidebar/>
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         {children}
                     </main>
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.rtl.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'react-toastify/dist/ReactToastify.css'
 import '@/styles/globals.css'
 import {useEffect} from "react";
 import Layout from "@/components/layout/Layout";
+import {ToastContainer} from "react-toastify";
 
 
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.bundle.js')
-  }, []);
-  return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-  )
+function MyApp({Component, pageProps}) {
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap.bundle.js')
+    }, []);
+    return (
+        <Layout>
+            <Component {...pageProps} />
+            <ToastContainer/>
+        </Layout>
+    )
 }
 
 export default MyApp
